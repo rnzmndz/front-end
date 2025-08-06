db00be19ac376c630586d8a3c0fd65f03c2d5539
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-08-06 16:39:10 +0800

    update

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,7 +3,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  if (token) {
+  const publicPaths = ['/auth/login', '/auth/register'];
+
+  const isPublic = publicPaths.some((path) => req.url.includes(path));
+
+  if (token && !isPublic) {
     const cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
